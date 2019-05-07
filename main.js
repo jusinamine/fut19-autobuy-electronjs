5f7bf83906a24c3c9ab5fd5966b92b776f58e615
aminox <amine.benk27@gmail.com>
2019-05-07 11:50:59 +0100

    [master]: add flask server

--- main.js
+++ main.js
@@ -4,6 +4,62 @@ const path = require('path');
 var fs = require('fs');
 let mainWindow = null
 
+
+//flask and electron 
+const PY_DIST_FOLDER = 'apidist'
+const PY_FOLDER = 'api'
+const PY_MODULE = 'futApi' // without .py suffix
+
+let apiProc = null
+let apiPort = null
+
+const guessPackaged = () => {
+  const fullPath = path.join(__dirname, PY_DIST_FOLDER)
+  return require('fs').existsSync(fullPath)
+}
+
+const getScriptPath = () => {
+  if (!guessPackaged()) {
+    return path.join(__dirname, PY_FOLDER, PY_MODULE + '.py')
+  }
+  if (process.platform === 'win32') {
+    return path.join(__dirname, PY_DIST_FOLDER, PY_MODULE, PY_MODULE + '.exe')
+  }
+  return path.join(__dirname, PY_DIST_FOLDER, PY_MODULE, PY_MODULE)
+}
+
+const selectPort = () => {
+  apiPort = 5000
+  return apiPort
+}
+
+const createPyProc = () => {
+  let script = getScriptPath()
+  let port = '' + selectPort()
+
+  if (guessPackaged()) {
+    apiProc = require('child_process').execFile(script, [port])
+  } else {
+    apiProc = require('child_process').spawn('python', [script, port])
+  }
+ 
+  if (apiProc != null) {
+    //console.log(pyProc)
+    console.log('child process success on port ' + port)
+  }
+}
+
+const exitPyProc = () => {
+  apiProc.kill()
+  apiProc = null
+  apiPort = null
+}
+
+app.on('ready', createPyProc)
+app.on('will-quit', exitPyProc)
+
+
+
 const createWindow = () => {
   mainWindow = new BrowserWindow({width: 800, height: 600,minHeight:300,minWidth:400,webPreferences: {
     nodeIntegration: true
@@ -19,9 +75,9 @@ const createWindow = () => {
   })
 }
 
+// electron part
 app.on('ready', ()=>{
     createWindow();
-
     if (!fs.existsSync('./accounts.json')) {
         fs.writeFile('accounts.json','[{}]', function (err) {
             if (err) throw err;
@@ -57,3 +113,4 @@ ipcMain.on('requestHandler', (event, data) => {
     });
   }
 });
+
